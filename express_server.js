e6e611bbb1c4aa055968dca96225463d2c3e4df0
Melissa Louette <melissalouette@Melissas-MacBook-Pro.local>
2019-03-13 15:01:03 -0400

    Added delete function

--- express_server.js
+++ express_server.js
@@ -4,11 +4,17 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 15);
+};
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));   
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,7 +37,38 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  //res.send(urlDatabase);      
+  res.redirect(`/urls/${shortURL}`); 
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log("TEST")
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  console.log('delete');
+  delete urlDatabase[shortURL];
+  res.redirect("/urls/");
+});
+//retrieving longURL from the form
+//generate new shortURL
+//save to urlDatabase
+
+//remove a URL resource
+//redirect client back to 
